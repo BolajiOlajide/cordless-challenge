ae039431dee14529b599957b8807a15f312ef618
Bolaji Olajide <cooproton@gmail.com>
2021-05-19 15:55:35 +0100

    feat: structure flask app

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DEBUG = os.getenv('DEBUG') == 'True'
+
---
+++ run.py
@@ -0,0 +1,11 @@
+from app import app
+import config
+
+
+if __name__ == "__main__":
+    # I modeled a real life production system here, so I set the host to 0.0.0.0
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+        debug=config.DEBUG
+    )
